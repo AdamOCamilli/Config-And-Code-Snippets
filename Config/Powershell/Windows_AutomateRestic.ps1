bd05585f74a096cb0edb6ec28966632219c4dd2b
Adam Camilli <a.o.camilli25@gmail.com>
2022-01-15 20:16:39 -0500

    Start of app project...

--- Config/Powershell/Windows_AutomateRestic.ps1
+++ Config/Powershell/Windows_AutomateRestic.ps1
@@ -7,14 +7,26 @@
 	The name of the credential containing restic repository path and password.
 .PARAMETER Backup
 	Path to backup
-.PARAMETER Log
-	Optional desired location of log. Located by default in parent directory of repository in file "AutomationLog.txt"
+.PARAMETER Commands
+	Restic commands to execute. PATHS MUST BE RELATIVE TO BACKUP PATH!
+.PARAMETER LogLocation
+	Optional desired location of log. Located by default in parent directory of repository
+.PARAMETER LogName
+	Optional desired name of log. Default name is "AutomationLog.txt"
 .NOTES
 	To automate: 
 		Create a scheduled task with desired schedule where 
 			Action = "Start a program"
-			Program/script = "powershell"
-			Add arguments (optional) = "<path to script>\Windows_AutomateRestic.ps1 -Credential <Name of restic credential> -Backup <path to backup>"
+			Program/script = powershell
+			Add arguments = <path to script>\Windows_AutomateRestic.ps1 -Credential <Name of restic credential> -Backup <path to backup> -Commands <commands>
+		Formatting arguments field for Task Scheduler using outer and inner quotes is annoying to say the least. The best way I have found to escape inner quotes for commands flag is to use single quotes as the outer quotes and escaped double quotes as the inner quotes, e.g.:
+			<...> -Commands '<command> \"<arg that must be quoted>\" ; <more commands...>'
+		Restic commands and their flags can thereby be ordered as necessary. 
+		EXAMPLE: 
+		Backup home folder (where you should probably exclude AppData folder), excluding a folder named "Unimportant Stuff", follow a forget policy, and prune unused data:
+			<...> -Backup ~ <...> -Commands 'restic backup . --iexclude ./AppData/ --iexclude \"./Unimportant Stuff/\" ; restic forget -w 1; restic unlock; restic prune'
+														   ^															 					 ^
+														   Relative to backup path 										 					 Remove stale locks before pruning
 	Author: Adam Camilli
 	Date: 1/10/2022
 #>
@@ -26,20 +38,64 @@ param (
 	[Parameter(Mandatory, HelpMessage="Path to back up")]
     [string]$Backup,
 	
-	[Parameter(HelpMessage="Optionally set location of LogFile (default is parent dir of repository)")]
-	[string]$Log=""
+	[Parameter(Mandatory, HelpMessage="Set restic commands to execute. Separate by `";`". PATHS MUST BE RELATIVE TO BACKUP PATH!")]
+	[string]$Commands,
+	
+	[Parameter(HelpMessage="Optionally set location of log file (default is parent dir of repository)")]
+	[string]$LogLocation="",
+	
+	[Parameter(HelpMessage="Optionally set name of log file (default is AutomationLog.txt)")]
+	[string]$LogName=""
 )
+
+# Parse a restic command for flags and verify it is a valid restic command
+Function isValidResticCommand 
+{
+	Param([string]$Command)
+	
+	$rcTokenized = $Command.Trim() -split " "
+	if ($rcTokenized[0] -ne "restic") { return $false }
+	return $true
+}
+
+# Execute restic commands
+Function executeResticCommands
+{
+	Param([string]$CommandStr, [string]$LogFile)
+	
+	$ResticCommands = $CommandStr -split ";"
+	
+	foreach ($resticCommand in $ResticCommands)
+	{
+		if (isValidResticCommand($resticCommand)) 
+		{
+			$sw = [Diagnostics.Stopwatch]::StartNew()
+			("#" + " BEGIN COMMAND: `"{0}`" " -f $resticCommand.Trim() + "#") | Out-File -FilePath $LogFile -Append
+			$OutputToLog = " 1>> {0} 2>&1" -f $LogFile
+			$fullCommand = $resticCommand + $OutputToLog
+			Invoke-Expression $fullCommand 
+			$sw.Stop()
+			("#" + "  END COMMAND. Total time of operation: {0} " -f $sw.Elapsed + "#") | Out-File -FilePath $LogFile -Append
+		} 
+		else 
+		{
+			("#" + " INVALID COMMAND: `"{0}`" " -f $resticCommand.Trim() + "#") | Out-File -FilePath $LogFile -Append
+		}
+	}
+
+}
 
 $sw = [Diagnostics.Stopwatch]::StartNew()
 
 $RepoPath = $(Get-StoredCredential -Target $Credential).UserName
 $RepoPassword = $(Get-StoredCredential -Target $Credential).GetNetworkCredential().Password
 
-if (-not $Log) {
+if (-not $Log) 
+{
 	$Log = (Get-Item $RepoPath).parent.FullName
 }
-$LogFile = $Log + "\AutomationLog.txt"
-("START automatic backup for {0} on {1}" -f $Credential, (Get-Date).date) | Out-File -FilePath $LogFile -Append
+$LogFile = $Log + "\TestAutomationLog.txt"
+("#" * 4 + " START restic run for {0} on {1} " -f $Credential, (Get-Date) + "#" * 4) | Out-File -FilePath $LogFile -Append
 
 if (-not $env:RESTIC_REPOSITORY) {
 	("Set Restic environment variables for the {0} repository." -f $Credential) | Out-File $LogFile -Append
@@ -53,14 +109,15 @@ $env:RESTIC_PASSWORD = $RepoPassword
 # Go to backup directory in order to use relative paths for restic backup
 cd $Backup
 
-"BEGIN Restic Output: " | Out-File $LogFile -Append
-restic backup --iexclude ./AppData . 1>> $LogFile 2>&1 | Out-Null # Wait for job to finish otherwise Powershell will terminate
-"END Restic Output " | Out-File $LogFile -Append
+("#" * 2 + " BEGIN Restic Run: " + "#" * 2) | Out-File $LogFile -Append
+executeResticCommands -CommandStr $Commands -LogFile $LogFile | Out-Null # Wait for job to finish otherwise Powershell will terminate
+#restic backup --iexclude ./AppData --iexclude ./Pictures/4chan/noice --iexclude "./Pictures/4chan/new noice" . 1>> $LogFile 2>&1 | Out-Null # Wait for job to finish otherwise Powershell will terminate
+("#" * 2 + " END Restic Run. " + "#" * 2) | Out-File $LogFile -Append
 
 ri env:RESTIC_REPOSITORY
 ri env:RESTIC_PASSWORD
 "Successfully unset Restic Env variables" | Out-File $LogFile -Append
 
 $sw.Stop()
-("END successful automatic backup for {0}. Total time of operation: {1}" -f $Credential, $sw.Elapsed) | Out-File $LogFile -Append
+("#" * 4 + " END successful restic run for {0}. Total time of operation(s): {1} " -f $Credential, $sw.Elapsed + "#" * 4)  | Out-File $LogFile -Append
 
